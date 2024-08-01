c67aa3361aff7fbd7d8c279eaa2f4408b169e7b5
Drew Poulin <drewpcchannel@gmail.com>
2024-08-01 17:03:37 -0400

    more checks for files already there

--- SetWallpaper.ps1
+++ SetWallpaper.ps1
@@ -10,7 +10,7 @@ if (Test-Path -Path .\wps.zip)
 
 #download wallpaper \ lock
 Invoke-WebRequest https://github.com/drewpchannel/setWallPaper/archive/refs/heads/main.zip -OutFile .\wps.zip
-Expand-Archive .\wps.zip
+Expand-Archive .\wps.zip -Force
 
 #get machine username, user has to be logged in or the hive might not be mounted
 $SID = & ".\wps\setWallPaper-main\SIDget\GetSID.ps1"
@@ -19,7 +19,5 @@ $SID = & ".\wps\setWallPaper-main\SIDget\GetSID.ps1"
 $filePath = Resolve-Path ".\wps\setWallPaper-main"
 
 Set-ItemProperty -Path "Registry::HKEY_USERS\$SID\Control Panel\Desktop" -Name WallPaper -value "$filePath\Treeline Wallpaper.png"
-cmd.exe "/c rundll32.exe user32.dll, UpdatePerUserSystemParameters 1, True"
 
-Remove-Item .\wps -Recurse
 Remove-Item .\wps.zip
